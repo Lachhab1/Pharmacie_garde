5c0b9b409214cf0b81d966bb9de8e852aedc36ec
Lachhab1 <Mohamed172lachhab@gmail.com>
2024-01-04 01:17:50 +0100

    co

--- src/components/pharmacyGarde/gard.jsx
+++ src/components/pharmacyGarde/gard.jsx
@@ -33,23 +33,7 @@ export default function Garde() {
             console.log(error);
         }
     };
-    const pharmacies = [
-        {
-            name: "Pharmacie 1",
-            isGarde: true,
-            image: { img1 }
-        },
-        {
-            name: "Pharmacie 2",
-            isGarde: false,
-            image: "https://images.unsplash.com/photo-1612838250290-6d5c3e6d9f6a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGhhcm1hY3klMjB3aWRnZXQlMjBwaG9uZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
-        },
-        {
-            name: "Pharmacie 3",
-            isGarde: false,
-            image: "https://images.unsplash.com/photo-1612838250290-6d5c3e6d9f6a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGhhcm1hY3klMjB3aWRnZXQlMjBwaG9uZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
-        },
-    ]
+
     return (
         <>
             <div className="flex flex-wrap justify-start">
